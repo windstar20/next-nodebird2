97bbe19a61a6a90027a91ae6d71f1123a8d949f1
james <james@irmkr>
2021-11-03 18:17:26 +0900

    1. Features: [4-12] front 팔로우, 언팔로우 구현2

--- prepare/front/sagas/user.js
+++ prepare/front/sagas/user.js
@@ -1,18 +1,26 @@
 import { all, delay, fork, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 import {
+    FOLLOW_FAILURE,
+    FOLLOW_REQUEST,
+    FOLLOW_SUCCESS,
     LOG_IN_FAILURE,
     LOG_IN_REQUEST,
     LOG_IN_SUCCESS,
     LOG_OUT_FAILURE,
     LOG_OUT_REQUEST,
-    LOG_OUT_SUCCESS, SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS
-} from "../reducers/user";
+    LOG_OUT_SUCCESS,
+    SIGN_UP_FAILURE,
+    SIGN_UP_REQUEST,
+    SIGN_UP_SUCCESS,
+    UNFOLLOW_FAILURE,
+    UNFOLLOW_REQUEST,
+    UNFOLLOW_SUCCESS
+} from '../reducers/user';
 
 function logInAPI(){
-    return axios.post('/api/login')
+    return axios.post('/api/login');
 }
-
 
 function* logIn(action) {
     console.log('사가 실행', action);
@@ -29,7 +37,7 @@ function* logIn(action) {
         yield put({
             type: LOG_IN_FAILURE,
             error: err.response.data, //실패 결과 값
-        })
+        });
     }
 }
 
@@ -68,7 +76,49 @@ function* sighUp() {
         yield put({
             type: SIGN_UP_FAILURE,
             error: err.response.data,
-        })
+        });
+    }
+}
+
+function followAPI() {
+    return axios.post('/api/follow');
+}
+
+function* follow(action) {
+    try {
+        // yield call(logOutApi);
+        yield delay(1000);
+        // throw new Error('');
+        yield put({
+            type: FOLLOW_SUCCESS,
+            data: action.data,
+        });
+    } catch (err) {
+        yield put({
+            type: FOLLOW_FAILURE,
+            error: err.response.data,
+        });
+    }
+}
+
+function unfollowAPI() {
+    return axios.post('/api/unfollow');
+}
+
+function* unfollow(action) {
+    try {
+        // yield call(logOutApi);
+        yield delay(1000);
+        // throw new Error('');
+        yield put({
+            type: UNFOLLOW_SUCCESS,
+            data: action.data,
+        });
+    } catch (err) {
+        yield put({
+            type: UNFOLLOW_FAILURE,
+            error: err.response.data,
+        });
     }
 }
 
@@ -84,12 +134,19 @@ function* watchLogOut() {
 function* watchSignUp() {
     yield takeLatest(SIGN_UP_REQUEST, sighUp);
 }
+function* watchFollow() {
+    yield takeLatest(FOLLOW_REQUEST, follow);
+}
+function* watchUnFollow() {
+    yield takeLatest(UNFOLLOW_REQUEST, unfollow);
+}
 
 export default function* userSaga() {
     yield all([
+        fork(watchFollow),
+        fork(watchUnFollow),
         fork(watchLogIn),
         fork(watchLogOut),
         fork(watchSignUp),
-    ])
-
+    ]);
 }
